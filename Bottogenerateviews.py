bc0d52e1c3515fe0ce639ecd5b3b4939e13a73ed
Sreetam Ganguly <32910454+Sreetam@users.noreply.github.com>
2018-08-04 20:06:09 +0530

    Update Bottogenerateviews.py

--- Bottogenerateviews.py
+++ Bottogenerateviews.py
@@ -5,12 +5,12 @@ from torrequest import TorRequest
 import numpy.random as nr
 import urllib3
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #there will be warnings. but be sure to trust the website befor doing this
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
 }
 
-proxyPort=8080
+proxyPort=8080 #default tor config
 ctrlPort=9051
 site = ["https://sreetamspeaks.blogspot.com/2017/02/it-fascinates-me-to-read-about-lives-of.html",
         "https://sreetamspeaks.blogspot.com/2018/08/why-wont-you-just-sleep.html",
@@ -21,9 +21,9 @@ site = ["https://sreetamspeaks.blogspot.com/2017/02/it-fascinates-me-to-read-abo
         "https://sreetamspeaks.blogspot.com/2017/02/james-hogg-was-well-known-poet-but-he.html",
         "https://sreetamspeaks.blogspot.com/2017/02/sitting-alone-my-mind-oftenwanders-to.html",
         "https://sreetamspeaks.blogspot.com/2017/01/he-woke-up-in-this-morning-looked_19.html",
-        "https://sreetamspeaks.blogspot.com/2017/01/thick-and-dark-clouds-sometimes.html"]
+        "https://sreetamspeaks.blogspot.com/2017/01/thick-and-dark-clouds-sometimes.html"]  #use a longer list to avoid detection
 
-blog = 3000000
+blog = 3000000 # this is the number of times the website needs to be pinged
 
 if __name__ == '__main__':
     if len(sys.argv) > 3:
@@ -34,5 +34,5 @@ if __name__ == '__main__':
         for i in range(blog):
             response = tr.get(site[nr.randint(0,10)], headers=headers,verify=False)
             print ("Blog View "+str(i)+" Added With IP:" +str(tr.get('http://ipecho.net/plain').content))
-            #time.sleep(nr.randint(60,100))
+            #time.sleep(nr.randint(60,100))  #for avoiding detection
             tr.reset_identity()
